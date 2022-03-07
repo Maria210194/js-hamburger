351e3e5e87428ef6b34f6f56263c9f3d41cbc05c
Maria Stachie <mary.stachie@gmail.com>
2022-03-07 23:58:26 +0100

    done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 console.log('JS OK!');
 
-let dropMenu = document.querySelector('.header-right');
+let dropMenu = document.querySelector('.header-right > a');
 let closeMenu = document.querySelector('.close');
 let hamburger = document.querySelector('.hamburger-menu');
+/*
+dropMenu.addEventListener('click', function () {
+    hamburger.style.display = 'block';
+}
+);
+
+closeMenu.addEventListener('click', function () {
+    hamburger.style.display = "none";
+}
+);*/
 
 dropMenu.addEventListener('click', function () {
     hamburger.classList.add('active');
