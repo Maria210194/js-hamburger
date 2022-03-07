5a0d44c479b5b39d6a49de569f94ef027ff44dba
Maria Stachie <mary.stachie@gmail.com>
2022-03-07 22:48:43 +0100

    test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 console.log('JS OK!');
+
+let dropMenu = document.querySelector('.header-right');
+let closeMenu = document.querySelector('.close');
+let hamburger = document.querySelector('.hamburger-menu');
+
+dropMenu.addEventListener('click', function () {
+    hamburger.classList.add('active');
+}
+);
+
+closeMenu.addEventListener('click', function () {
+    hamburger.classList.remove('active');
+}
+);
